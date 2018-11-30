2063424f39a531251c1ebd11d083c94da691cca8
Ed Putans <edputans@gmail.com>
2018-11-30 13:33:16 +0000

    n

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
+
 import MainPage from "./components/MainPage";
 
 class App extends React.Component {
