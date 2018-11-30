e063ff9b233399b02c7e4f6a71baf628237c22ac
Ed Putans <edputans@gmail.com>
2018-11-30 10:50:03 +0000

    created a few routes before checking out aishas branch

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Route, Switch} from 'react-router-dom'
 import PieChart from "react-minimal-pie-chart";
 import { Grid } from "semantic-ui-react";
 
@@ -6,13 +7,23 @@ import Login from "./Login";
 import Signup from "./Signup";
 import SideBar from "./SideBar";
 import SpendingPage from "./SpendingPage";
+import HomePage from "./HomePage";
 
 export default class MainPage extends React.Component {
   render() {
     return (
       <div>
-        {/* Divided in Grid is used for the splitting line between the sidebar and main page */}
+          <Switch>
+        {/* userLoggedin is false? 
         
+    
+    
+    
+           otherwise    */}
+            <Route exact path='/' component={ HomePage } />
+            <Route exact path='/login' component={ Login } />
+            <Route exact path='/Signup' component={ Signup } />
+            
         <Grid columns={2} divided>
           <Grid.Row>
             <Grid.Column width={12}>
@@ -23,6 +34,8 @@ export default class MainPage extends React.Component {
             </Grid.Column>
           </Grid.Row>
         </Grid>
+
+        </Switch>
       </div>
     );
   }
