8ba8e28bfa3449e9d2157cb7acd6911401d0a0f4
Ed Putans <edputans@gmail.com>
2018-12-02 14:06:56 +0000

    positively renders/changes the objective amount

--- src/components/ObjectivePage.js
+++ src/components/ObjectivePage.js
@@ -11,47 +11,72 @@ export default class ObjectivePage extends React.Component {
 
 state={
     objective: '',
-    objectives: []
+    objectives: [],
+    value: 0
 }
 
 
+handleChange = val =>{
+    let value = parseInt(val)
+    this.setState({value})
+}
 
+handleSubmit = () =>{
+    let newObjective = JSON.stringify(this.state.objective)
+    newObjective = JSON.parse(newObjective)
+    newObjective.current_amount = this.state.value
+    console.log('new',newObjective)
+    this.patchObjective(newObjective)
+    this.setState({ objective: newObjective})
+}
+
+    componentDidMount() {
+        let objectiveId = this.props.match.params.id
+        this.fetchObjective(objectiveId)
+    }
+
+    // from adapter
 fetchObjective = objectiveId => {
     return fetch(`${objectivesURL}/${objectiveId}`).then(resp => resp.json()).then(objective => this.setState({objective}))
-};
-
-componentDidMount(){   
-let objectiveId = this.props.match.params.id
-this.fetchObjective(objectiveId)
 }
 
-
-    render() {
-        const {objective} = this.state
-        let current_percentage = (objective.current_amount/objective.total_amount)*100
-        let remaining_percentage = 100-current_percentage
-        return <div>
-            <PieChart
-                lineWidth="10"
-                size="small"
-                ratio="1"
-                style={ { height: "300px" } }
-                data={ [
-                    { title: "One", value: current_percentage, color: "#00FF00" },
-                    { title: "Two", value: remaining_percentage, color: "#FF6347" },
-                ] }
-            />
-            <br></br>
-            <Form>
-                <Header as='h1' >Goal: £{objective.total_amount} :) </Header>
-                <Header as='h1' >Currently saved: £{objective.current_amount} </Header>
-                <Form.Input placeholder="Set new amount?" step="10" type='number' 
-                onChange={this.handleChange}/>
-                <Button className="ui purple basic button" onClick={this.handleSubmit}>Submit</Button>
-            </Form>
-            <a href='/spending'>Back to Main page</a>
+patchObjective = objective => {
+    fetch(`${objectivesURL}/${objective.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(objective)
+    }).then(resp => resp.json())
+}
+    // 
 
 
-        </div>
-    }
+
+render() {
+    const {objective} = this.state
+    let current_percentage = (objective.current_amount/objective.total_amount)*100
+    let remaining_percentage = 100-current_percentage
+    return <div>
+        <PieChart
+            lineWidth="10"
+            size="small"
+            ratio="1"
+            style={ { height: "300px" } }
+            data={ [
+                { title: "One", value: current_percentage, color: "#00FF00" },
+                { title: "Two", value: remaining_percentage, color: "#FF6347" },
+            ] }
+        />
+        <br></br>
+        <Form>
+            <Header as='h1' >Goal: £{objective.total_amount} :) </Header>
+            <Header as='h1' >Currently saved: £{objective.current_amount} </Header>
+            <Form.Input placeholder="Set new amount?" step="0.1" type='number' 
+            onChange={event => this.handleChange(event.target.value)}/>
+            <Button className="ui purple basic button" onClick={this.handleSubmit}>Submit</Button>
+        </Form>
+        <a href='/spending'>Back to Main page</a>
+    </div>
+}
 }
