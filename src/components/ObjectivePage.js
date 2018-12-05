4800e054bd4664411b49d4927167fe82ae1a6f88
Ed Putans <edputans@gmail.com>
2018-12-05 10:26:55 +0000

    successfully merged

--- src/components/ObjectivePage.js
+++ src/components/ObjectivePage.js
@@ -1,110 +1,116 @@
-import React from 'react'
-import PieChart from 'react-minimal-pie-chart'
-import { Form, Button, Header } from 'semantic-ui-react'
+import React from "react";
+import { Form, Button, Header } from "semantic-ui-react";
+import SpendingPieChart from "./SpendingPieChart";
+import ObjectiveTable from "./ObjectiveTable";
+import { Table } from "semantic-ui-react";
 
 const objectivesURL = "http://localhost:3000/api/v1/objectives";
 const usersURL = "http://localhost:3000/api/v1/users";
 
-
 // This is the page that we will use for the objective data
 export default class ObjectivePage extends React.Component {
+    state = {
+        objective: "",
+        objectives: [],
+        value: 0,
+        users: [],
+        amount: []
+    };
 
+    handleChange = val => {
+        let value = parseInt(val);
+        this.setState({ value });
+    };
 
-state={
-    objective: '',
-    objectives: [],
-    value: 0,
-    users:[]
-}
+    handleSubmit = () => {
+        if (this.state.value <= this.state.objective.total_amount && this.state.value >= 0){
+            let newObjective = JSON.stringify(this.state.objective);
+            newObjective = JSON.parse(newObjective);
+            newObjective.current_amount = this.state.value;
+            console.log("new", newObjective);
+            this.patchObjective(newObjective);
+            this.setState({ objective: newObjective });
+        } else {
+            alert("Please provide a valid value!")
+        }
+    };
 
+    componentDidMount() {
+        let objectiveId = this.props.match.params.id;
+        this.fetchObjective(objectiveId);
+        this.fetchUsers().then(users => this.setState({ users }));
+    }
 
-handleChange = val =>{
-    let value = parseInt(val)
-    this.setState({value})
-}
-
-handleSubmit = () =>{
-    let newObjective = JSON.stringify(this.state.objective)
-    newObjective = JSON.parse(newObjective)
-    newObjective.current_amount = this.state.value
-    console.log('new',newObjective)
-    this.patchObjective(newObjective)
-    this.setState({ objective: newObjective})
-}
-
-componentDidMount() {
-    console.log(this.state.objective)  
-    console.log(this.props.location.state.objective)
-    let objectiveId = this.props.match.params.id
-    this.fetchObjective(objectiveId)
-    this.fetchUsers().then(users=> this.setState({ users }))
-}
-
-
-componentUpdate(){
-    console.log(this.state.objective)   
-}
+    componentUpdate() {
+        console.log(this.state.objective);
+    }
 
     // from adapter
-fetchObjective = objectiveId => {
-    console.log('clicked fetch objective thing')
-    return fetch(`${objectivesURL}/${objectiveId}`).then(resp => resp.json()).then(objective => this.setState({objective}))
-}
+    fetchObjective = objectiveId => {
+        console.log("clicked fetch objective thing");
+        return fetch(`${objectivesURL}/${objectiveId}`)
+            .then(resp => resp.json())
+            .then(objective => this.setState({ objective }));
+    };
 
-getObjectiveFromLocation = () =>{
-    const {objective} = this.props.location.state
-    this.setState({ objective })
+    getObjectiveFromLocation = () => {
+        const { objective } = this.props.location.state;
+        this.setState({ objective });
+    };
 
-}
+    setAmount() {
+        const obj = this.state.objective;
+        let res = [];
+        for (const key in obj) {
+            if (key === "id" || key === "user" || key === "name") {
+                continue;
+            }
+            res.push([key.charAt(0).toUpperCase() + key.slice(1), obj[key]]);
+        }
+        return res;
+    }
 
-patchObjective = objective => {
-    fetch(`${objectivesURL}/${objective.id}`, {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(objective)
-    }).then(resp => resp.json())
-}
+    patchObjective = objective => {
+        fetch(`${objectivesURL}/${objective.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(objective)
+        }).then(resp => resp.json());
+    };
     //
 
+    fetchUsers = () => {
+        return fetch(`${usersURL}/`).then(resp => resp.json());
+    };
 
-
-fetchUsers = () => {
-    return fetch(`${usersURL}/`).then(resp => resp.json())
-};
-
-
-
-
-render() {
-    const {objective, value} = this.state
-    let current_percentage = (objective.current_amount/objective.total_amount)*100
-    let remaining_percentage = 100-current_percentage
-    return <div>
-        <PieChart
-            lineWidth={10}
-            style={ { height: "300px" } }
-            data={ [
-                { title: "One", value: current_percentage, color: "#00FF00" },
-                { title: "Two", value: remaining_percentage, color: "#FF6347" },
-            ] }
-        />
-        <br></br>
-        <Form>
-            <Header as='h1' >Goal: £{objective.total_amount} :) </Header>
-            <Header as='h1' >Currently saved: £{objective.current_amount} </Header>
-            <Form.Input placeholder="Set new amount?" type='float'
-            onChange={event => this.handleChange(event.target.value)}/>
-            <Button className="ui purple basic button"
-                    onClick=
-                    {value <= objective.total_amount && value >= 0?
-                        () => this.handleSubmit()
-                        :
-                        () => alert("Please provide a valid value!")
-                }>Submit</Button>
-        </Form>
-        <a href='/'>Back to Main page</a>
-    </div>
-}
+    render() {
+        const objectiveData = this.setAmount();
+        const { objective, value } = this.state;
+        let current_percentage =
+            (objective.current_amount / objective.total_amount) * 100;
+        let remaining_percentage = 100 - current_percentage;
+        return (
+            <div>
+                <h2>{ this.state.objective.name }</h2>
+                { objectiveData.length !== 0 ? (
+                    <SpendingPieChart data={ objectiveData } />
+                ) : null }
+                <br />
+                <div>
+                    { objectiveData.length !== 0 ? (
+                        <ObjectiveTable
+                            handleSubmit={ this.handleSubmit }
+                            item={ objectiveData }
+                            handleChange={ this.handleChange }
+                            handleSubmit={ this.handleSubmit }
+                        />
+                    ) : null }
+                </div>
+                 
+                
+            </div>
+        );
+    }
 }
