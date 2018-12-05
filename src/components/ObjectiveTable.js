2ad8486cb541d368a397d76fed579b3060bf2779
wimbleclaw <aisha305@hotmail.co.uk>
2018-12-05 14:19:18 +0000

    added minus

--- src/components/ObjectiveTable.js
+++ src/components/ObjectiveTable.js
@@ -2,59 +2,73 @@ import React from "react";
 import { Table, Button } from "semantic-ui-react";
 
 export default class InputTableRow extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            input: ""
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    };
+  }
 
-    updateInput(input) {
-        this.setState({
-            input
-        });
-    }
+  updateInput(input) {
+    this.setState({
+      input
+    });
+  }
 
-    remainder() {
-        return this.props.item[1][1] - this.props.item[0][1];
-    }
+  remainder() {
+    return this.props.item[1][1] - this.props.item[0][1];
+  }
 
-    render() {
-        return (
-            <Table>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Total</Table.HeaderCell>
-                        <Table.HeaderCell>Current Amount</Table.HeaderCell>
-                        <Table.HeaderCell>Remainder</Table.HeaderCell>
-                        <Table.HeaderCell>Add</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    <Table.Row>
-                        <Table.Cell>£ { this.props.item[1][1] }</Table.Cell>
-                        <Table.Cell>£ { this.props.item[0][1] }</Table.Cell>
-                        <Table.Cell>£ { this.remainder() }</Table.Cell>
-                        <Table.Cell selectable>
-                            <label>
-                                Add:
-                             <input
-                                    type="number"
-                                    name="name"
-                                    onChange={ event => this.props.handleChange(event.target.value) }
-                                />
-                                <Button
-                                    className="ui purple basic button"
-                                    onClick={ () => this.props.handleSubmit()
-                                    }
-                                >
-                                    Submit
-                </Button>
-                            </label>
-                        </Table.Cell>
-                    </Table.Row>
-                </Table.Body>
-            </Table>
-        );
-    }
+  clear() {
+    this.input.val = "";
+  }
+
+  render() {
+    return (
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Total</Table.HeaderCell>
+            <Table.HeaderCell>Current Amount</Table.HeaderCell>
+            <Table.HeaderCell>Remainder</Table.HeaderCell>
+            <Table.HeaderCell>Add</Table.HeaderCell>
+            <Table.HeaderCell>Reduce</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>£ {this.props.item[1][1]}</Table.Cell>
+            <Table.Cell>£ {this.props.item[0][1]}</Table.Cell>
+            <Table.Cell>£ {this.remainder()}</Table.Cell>
+            <Table.Cell selectable>
+              <input
+                type="number"
+                name="name"
+                onChange={event => this.props.handleChange(event.target.value)}
+              />
+              <Button
+                className="ui purple basic button"
+                onClick={() => this.props.handleAdd()}
+              >
+                +
+              </Button>
+            </Table.Cell>
+            <Table.Cell selectable>
+              <input
+                type="number"
+                name="name"
+                onChange={event => this.props.handleChange(event.target.value)}
+              />
+              <Button
+                className="ui purple basic button"
+                onClick={() => this.props.handleSubtract()}
+              >
+                -
+              </Button>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    );
+  }
 }
